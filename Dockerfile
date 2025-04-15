1c87d32d9c1a5a87d4860d2f29a2a877b7a5fa12
Hangrui Bi <hangruibi@outlook.com>
2025-04-14 23:00:59 -0400

    updated setup.py

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN git clone https://github.com/zhaoyu-li/PyEuclid
 # RUN mkdir /PyEuclid
 # COPY ./ /PyEuclid
 WORKDIR /PyEuclid
-RUN pip install -r requirements.txt
+RUN pip install .
 RUN tar -xvzf cache.tar.gz
 CMD python test_single.py
--- setup.py
+++ setup.py
@@ -6,5 +6,14 @@ setup(
     version='1.0.0',
     description='PyEuclid: A Versatile Formal Plane Geometry System in Python',
     packages=find_packages(),
+    install_requires=[  
+        "gurobipy",
+        "matplotlib",
+        "numpy",
+        "stopit",
+        "sympy",
+        "tqdm",
+        "z3-solver"
+    ], 
     include_package_data=True,
 )
