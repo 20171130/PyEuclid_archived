d4da019d79600631bfa0b4258dcf86e33ce907ba
zhaoyu-li <zhaoyuli@meta.com>
2025-05-27 19:17:56 +0000

    update

--- slurm.sh
+++ slurm.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+#SBATCH --job-name=JGEX_AG_231
 #SBATCH -n 30
+#SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
+
 export PYTHONBREAKPOINT=0
 
+conda activate pyeuclid
 mpirun python3 test.py
