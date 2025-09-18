6a08cbf3260268c618d63187a6de0ecd5af2bc55
youyuan-zhang <zhangyouyuan009@gmail.com>
2025-09-18 01:54:47 -0400

    update

--- scripts/informalize_template.py
+++ scripts/informalize_template.py
@@ -4,8 +4,8 @@ import shutil
 from pathlib import Path
 from pyeuclid.informalization.informalize_utils import *
 
-dataset_dir = "synthetic_dataset_0"
-dst_dataset_dir = "informalized_dataset"
+dataset_dir = "task2"
+dst_dataset_dir = "task2_template"
 data_json_list = []
 image_file_list = []
 for entry in Path(dataset_dir).rglob("*data.json"):
@@ -14,10 +14,6 @@ for entry in Path(dataset_dir).rglob("*data.json"):
     image_file_list.append(os.path.join(sample_dir, "diagram.jpg"))
 print("begin")
 
-# data_json_list = ["synthetic_dataset_0/rank_4/problem_0/sample_0/data.json"]
-# image_file_list = ["synthetic_dataset_0/rank_4/problem_0/sample_0/diagram.jpg"]
-# data_json_list = ["auxiliary_dataset/rank_12/problem_2/sample_0/data.json"]
-# image_file_list = ["auxiliary_dataset/rank_12/problem_2/sample_0/diagram.jpg"]
 total_num = len(data_json_list)
 error_list = list()
 result = dict()
@@ -32,7 +28,7 @@ for idx, (data_json, image_file) in enumerate(zip(data_json_list, image_file_lis
         continue
     problem = data["problem"]
     goal = data["goal"]
-    aux = data["auxiliary_constructions"]
+    # aux = data["auxiliary_constructions"]
     proof = data["proof"]
 
     informal_problem = informalize_problem(problem)
@@ -43,12 +39,13 @@ for idx, (data_json, image_file) in enumerate(zip(data_json_list, image_file_lis
     data["informal_goal"] = informal_goal
     # print(informal_goal)
 
-    informal_aux = informalize_aux(aux)
-    data["informal_aux"] = informal_aux
-    print(informal_aux)
+    # informal_aux = informalize_aux(aux)
+    # data["informal_aux"] = informal_aux
+    # print(informal_aux)
     
     informal_proof = informalize_proof(proof)
-    data["informal_proof"] = informal_aux + informal_proof
+    # data["informal_proof"] = informal_aux + informal_proof
+    data["informal_proof"] = informal_proof
     # print(informal_proof)
 
     dst_data_json = data_json.replace(dataset_dir, dst_dataset_dir)
@@ -56,12 +53,10 @@ for idx, (data_json, image_file) in enumerate(zip(data_json_list, image_file_lis
     os.makedirs(os.path.dirname(dst_data_json), exist_ok=True)
     with open(dst_data_json, "w") as f:
         json.dump(data, f, indent=4)
-    # if os.path.islink(dst_image_file):
-    #     os.unlink(dst_image_file)
-    # elif os.path.exists(dst_image_file):
-    #     os.remove(dst_image_file)
-    # os.symlink(os.path.abspath(image_file), dst_image_file)
-
-    # exit()
+    if os.path.islink(dst_image_file):
+        os.unlink(dst_image_file)
+    elif os.path.exists(dst_image_file):
+        os.remove(dst_image_file)
+    os.symlink(os.path.abspath(image_file), dst_image_file)
 
 print(error_list)
